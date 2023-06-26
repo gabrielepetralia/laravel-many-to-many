b07629baa881a66d302c07c3a60458d3111eb645
gabrielepetralia <gabri.petralia03@gmail.com>
2023-06-26 20:49:48 +0200

    make Technology model and related migration

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -26,4 +26,9 @@ class Project extends Model
   {
     return $this->belongsTo(Type::class);
   }
+
+  public function technologies()
+  {
+    return $this->belongsToMany(Technology::class);
+  }
 }
